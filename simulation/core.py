dcb31ab4e120fcfeec8f5f35150ef9424bc578c2
Manuel Zander <manuelzander@dyn3136-119.wlan.ic.ac.uk>
2018-06-05 18:17:02 +0100

    Work on simulation class

--- simulation/core.py
+++ simulation/core.py
@@ -1,8 +1,6 @@
 import helpers
 import constants
 from simulation import Simulation
-from agent import Agent
-from transaction import Transaction
 from plot import Plot
 
 try:
@@ -15,7 +13,6 @@ try:
     # plot = Plot(arrival_times)
     # plot.show_plot()
 
-    trans = Transaction()
 except Exception as e:
     print(e)
 
--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,11 +1,16 @@
+import sys
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import helpers
 import constants
+from agent import Agent
+from transaction import Transaction
 
 NO_OF_TRANSACTIONS = 10
 LAMDBA = 2
+NO_OF_AGENTS = 1
+DISTANCE = 5
 
 class Simulation:
     def __init__(self):
@@ -23,6 +28,15 @@ class Simulation:
         try:
             arrival_times = self.cum_random_values
             print(arrival_times)
+
+            if(NO_OF_AGENTS < 1):
+                print("The number of agents can not be less than 1")
+                sys.exit()
+            elif(NO_OF_AGENTS == 1):
+                DISTANCE = 0
+
+            trans = Transaction()
+
             '''
             DG.add_node(1)
             nx.draw(DG, with_labels=True)

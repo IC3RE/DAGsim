00058b8c9e855039905e12d1d28186142391d4b4
Manuel Zander <manuelzander@Manuels-MacBook-Pro.local>
2018-06-18 12:29:14 +0100

    Implemented unweigthed random walk

--- simulation/core.py
+++ simulation/core.py
@@ -5,7 +5,7 @@ from simulation import Simulation
 #Parameters: no_of_transactions, lambda, no_of_agents, alpha, latency, distance, tip_selection_algo
 #Tip selection algorithms are "random", "weighted", "unweighted"
 
-simu = Simulation(1000, 50, 1, 0, 1, 0, "unweighted")
+simu = Simulation(100, 50, 1, 0, 1, 0, "unweighted")
 
 simu.setup()
 
